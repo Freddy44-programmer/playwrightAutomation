66d1dce059f58274bb2e47d1a8326cc7eea3164a
freddy konyane <148207451+freddyKto@users.noreply.github.com>
2025-04-09 10:21:13 +0200

    added clientApp

---
+++ tests/ClientApp.spec.js
@@ -0,0 +1,16 @@
+const {test, expect} = require('@playwright/test');
+
+
+test.only('@WC Client App login', async ({page})=>
+    {
+ 
+     await page.goto("https://rahulshettyacademy.com/client");
+     await page.locator("#userEmail").fill("rangataft08@gmail.com");
+     await page.locator("#userPassword").fill("@12345Ft");
+     await page.locator("#login").click();
+    //  await page.waitForLoadState("networkidle");
+    await page.locator(".card-body b").first.waitFor();
+    const allTitles = await page.locator(".card-body b").allTextContents();
+    console.log(allTitles);
+   
+    });
--- tests/UIBasicstest.spec.js
+++ tests/UIBasicstest.spec.js
@@ -3,20 +3,27 @@ const {test, expect} = require('@playwright/test');
 
 test.only('Browser Context Playwright test', async ({browser})=>{
 
-//chrome - plugin/ cookies
 const context = await browser.newContext();
 const page = await context.newPage();
+const userName = page.locator('#username');
+const signIn = page.locator("#signInBtn");
+const cardTitles = page.locator(".card-body a");
 await page.goto("https://rahulshettyacademy.com/loginpagePractise/");
 console.log(await page.title());
-await page.goto("https://rahulshettyacademy.com/loginpagePractise/");
 //css
-await page.locator("#username").fill("shettyacademy");
+await userName.fill("shettyacademy");
 await page.locator("[type='password']").fill("learning");
-await page.locator("#signInBtn").click();
+await signIn.click();
 console.log(await page.locator("[style*='block']").textContent());
-
-
-
+await expect( page.locator("[style*='block']")).toContainText('Incorrect');
+//type - fill
+await userName.fill("");
+await userName.fill("rahulshettyacademy");
+await signIn.click();
+console.log(await cardTitles.first().textContent());
+console.log(await cardTitles.nth(1).textContent());
+const allTitles = await cardTitles.allTextContents();
+console.log(allTitles);
 
 });
 
