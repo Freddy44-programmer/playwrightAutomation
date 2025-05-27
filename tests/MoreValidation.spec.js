8af6a9c9492e4620bab845480b8932066e17d5a0
freddy konyane <148207451+freddyKto@users.noreply.github.com>
2025-05-27 10:38:38 +0200

    handling screenshot visual comparison

--- tests/MoreValidation.spec.js
+++ tests/MoreValidation.spec.js
@@ -24,4 +24,27 @@ const textCheck = await framesPage.locator(".text h2").textContent();
 textCheck.split(" ")[1];
 console.log(textCheck.split(" ")[1]);
 
-})
+});
+
+
+
+test.only('Screenshot & Visual comparison', async ({page})=>{
+await page.goto("https://rahulshettyacademy.com/AutomationPractice/");
+await expect(page.locator("#displayed-text")).toBeVisible();
+await page.locator("#displayed-text").screenshot({path: 'screenshot.png'});
+await page.locator("#hide-textbox").click();
+await page.screenshot({path: 'screenshot.png'});
+await expect(page.locator("#displayed-text")).toBeHidden();
+
+});
+
+//screenshot -store -> screenshot ->
+test('Visual', async ({page})=>{
+await page.goto("https://flightaware.com/");
+expect(await page.screenshot()).toMatchSnapshot('landing.png');
+await page.goto("https://rahulshettyacademy.com/upload-download-test/index.html");
+
+
+});
+
+
